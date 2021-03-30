f9400dd8750885142a1b2c5e89b677b0716fb9d3
leeonky <leeonky@gmail.com>
2021-03-30 13:44:45 +0000

    :white_check_mark: fix frontend profile config

--- frontend/src/api/request.js
+++ frontend/src/api/request.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // create an axios instance
 const service = axios.create({
-  baseURL: 'http://localhost:10080', // url = base url + request url
+  baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
   // withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000 // request timeout
 })
