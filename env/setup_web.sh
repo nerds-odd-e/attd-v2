4561138f6fe733b107204ceadaa19ef15c511d80
leeonky <leeonky@gmail.com>
2021-05-08 15:45:19 +0000

    debug ci

--- env/setup_web.sh
+++ env/setup_web.sh
@@ -4,9 +4,9 @@ project_path=$(dirname $0)
 
 HOSTS_FILE=/etc/hosts
 
-hosts_file=$(sudo docker exec web-driver cat /etc/hosts | grep -v frontend.app.net | grep -v backend.api.net)
+hosts_file=$(docker exec web-driver cat /etc/hosts | grep -v frontend.app.net | grep -v backend.api.net)
 
 echo "$hosts_file
 ${1} frontend.app.net
-${1} backend.api.net" | docker exec -i web-driver sudo sh -c "cat > /etc/hosts"
+${1} backend.api.net" | docker exec -i web-driver sh -c "cat > /etc/hosts"
 
