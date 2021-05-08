8d44d1b979b98b2e0ab67e7a121f2ec137939ca2
leeonky <leeonky@gmail.com>
2021-05-08 08:49:08 +0000

    update ci

--- env/setup_web.sh
+++ env/setup_web.sh
@@ -4,11 +4,9 @@ project_path=$(dirname $0)
 
 HOSTS_FILE=/etc/hosts
 
-IP=$(ping $1 -c 1 | head -n1 | awk -F\( '{print $2}' | awk -F\) '{print $1}')
-
 hosts_file=$(sudo docker exec web-driver cat /etc/hosts | grep -v frontend.app.net | grep -v backend.api.net)
 
 echo "$hosts_file
-${IP} frontend.app.net
-${IP} backend.api.net" | docker exec -i web-driver sudo sh -c "cat > /etc/hosts"
+${1} frontend.app.net
+${1} backend.api.net" | docker exec -i web-driver sudo sh -c "cat > /etc/hosts"
 
