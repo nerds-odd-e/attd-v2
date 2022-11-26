9b91ad9018563a03d2051755a0067f9f2121ff86
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2022-11-26 11:12:24 +0800

    :sparkles: add swagger ui for the token of all apis

---
+++ backend/src/main/java/com/odde/atddv2/config/OpenApi.java
@@ -0,0 +1,23 @@
+package com.odde.atddv2.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApi {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("token", new SecurityScheme()
+                                .type(SecurityScheme.Type.APIKEY)
+                                .in(SecurityScheme.In.HEADER)
+                                .name("token")))
+                .addSecurityItem(new SecurityRequirement().addList("token"));
+    }
+}
