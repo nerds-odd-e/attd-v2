a2699f30158282269d4c1346019ed7bfb3f1e6e5
Liangchen <leeonky@gmail.com>
2024-03-03 13:25:28 +0800

    Add redis example in order api

--- backend/src/main/java/com/odde/atddv2/controller/OrdersController.java
+++ backend/src/main/java/com/odde/atddv2/controller/OrdersController.java
@@ -7,6 +7,10 @@ import com.odde.atddv2.repo.OrderRepo;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.Clock;
@@ -28,6 +32,18 @@ public class OrdersController {
     @Autowired
     private Clock clock;
 
+    @Autowired
+    private RedisConnectionFactory redisConnectionFactory;
+
+    private RedisTemplate<String, Order.OrderTransaction> redisUserTemplate() {
+        RedisTemplate<String, Order.OrderTransaction> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer(Order.OrderTransaction.class));
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+
     @GetMapping
     @JsonView(GetAllOrders.class)
     public List<Order> getAllOrders() {
@@ -44,6 +60,7 @@ public class OrdersController {
     @JsonView(GetOrder.class)
     public Order getOrder(@PathVariable String code) {
         Order order = orderRepo.findByCode(code);
+        order.setTransaction(redisUserTemplate().opsForValue().get("TRANSACTION_" + order.getCode()));
         if (order.getDeliverNo() != null) {
             return order.populateLogistics(binstdApi.queryExpress(binstdAppKey, order.getDeliverNo()).getResult());
         } else {
